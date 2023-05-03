65f20a43a73e10fd0eb8b9396037a4337c31538b
Oleg Zavalin <oezavalin@gmail.com>
2023-05-03 07:54:53 +0300

    New app added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-date')[:5] # сортирует по дате и выводит последние 5
+    # или Blog.object.all() это все объекты
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
